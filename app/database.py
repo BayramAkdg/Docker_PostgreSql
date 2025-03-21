1ac251939ca2cc230e0647d748f7cb7390b50657
BayramAkdg <125415220+BayramAkdg@users.noreply.github.com>
2025-03-21 16:28:23 +0300

    database.py

--- app/database.py
+++ app/database.py
@@ -14,18 +14,22 @@ class Database:
 
     def connect(self):
         """Connect to the PostgreSQL database."""
+        
         try:
             database_url = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/library")
             self.connection = psycopg2.connect(database_url)
             self.cursor = self.connection.cursor()
             self.logger.info("Connected to the PostgreSQL database.")
+            
         except psycopg2.Error as e:
             self.logger.error(f"Error connecting to database: {e}")
 
     def close(self):
         """Close the database connection."""
+        
         if self.cursor:
             self.cursor.close()
+            
         if self.connection:
             self.connection.close()
             self.logger.info("Database connection closed.")
