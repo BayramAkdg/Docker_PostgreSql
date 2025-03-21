3023be5f4e7ba50c8e9e1b942b5ef005bac00b7a
BayramAkdg <125415220+BayramAkdg@users.noreply.github.com>
2025-03-21 17:23:55 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -3,25 +3,26 @@ from crud import BookManager
 from utility import LibraryUtility
 
 class LibraryApp:
-    def __init__(self):
+    def __init__(self, db):
         self.db = Database()
         self.db.connect()
         self.book_manager = BookManager(self.db)
         self.utility = LibraryUtility()
 
     def run(self):
-        """Run the Library Management System."""
+        """Run the Library Management System without using while loop."""
         self.utility.logger.info("Library Management System started.")
-        
-        while True:
-            choice = self.utility.show_menu()
-            
-            if choice == "5":
-                self.utility.logger.info("Exiting the system.")
-                self.db.close()
-                break
-                
+        self._process_user_choice()
+
+    def _process_user_choice(self):
+        """Process the user's choice in a recursive manner."""
+        choice = self.utility.show_menu()
+        if choice == "5":
+            self.utility._exit(self.book_manager)
+            self.db.close()
+        else:
             self.utility.handle_choice(choice, self.book_manager)
+            self._process_user_choice()
 
 if __name__ == "__main__":
     app = LibraryApp()
