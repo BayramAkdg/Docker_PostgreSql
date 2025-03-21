688fecbbe762ee5ec6720d62bf3202292c6d4c41
BayramAkdg <125415220+BayramAkdg@users.noreply.github.com>
2025-03-21 17:35:18 +0300

    database.py

--- app/database.py
+++ app/database.py
@@ -1,26 +1,28 @@
-import psycopg2
-import os
 import logging
+import psycopg2
 
 class Database:
-    def __init__(self):
+    def __init__(self, database_url="postgresql://user:password@localhost:5432/library"):
         self.connection = None
         self.cursor = None
+        self.database_url = database_url
         self._setup_logging()
 
     def _setup_logging(self):
+        """Setup logging configuration."""
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
         self.logger = logging.getLogger(__name__)
 
     def connect(self):
         """Connect to the PostgreSQL database."""
         try:
-            database_url = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/library")
-            self.connection = psycopg2.connect(database_url)
+            self.connection = psycopg2.connect(self.database_url)
             self.cursor = self.connection.cursor()
-            self.logger.info("Connected to the PostgreSQL database.")
-        except psycopg2.Error as e:
+            self.logger.info("Successfully connected to the database.")
+        except psycopg2.OperationalError as e:
             self.logger.error(f"Error connecting to database: {e}")
+            
+            raise
 
     def close(self):
         """Close the database connection."""
